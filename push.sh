1d4fbe948d9bcc410741506d6cbf5a9a97ef61dc
Emaleth <emaleth@protonmail.com>
2024-04-03 11:37:40 +0200

    test3

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-git add . && kdialog --title "Input dialog" --inputbox "Commit message:" > tmp.out && git commit -F tmp.out && git push || rm tmp.out
+git add . && kdialog --title "Input dialog" --inputbox "Commit message:" > tmp.out && git commit -F tmp.out && git push && rm tmp.out
