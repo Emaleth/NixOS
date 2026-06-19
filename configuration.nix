8b527422276b55b5337ab7a1462d934ae6b1118b
Emaleth <Emaleth@protonmail.com>
2026-06-20 00:26:08 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -159,12 +159,6 @@
         "flakes" 
       ];
       auto-optimise-store = true;
-      extra-substituters = [ "https://noctalia.cachix.org" ];
-      extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-    };
-    gc = {
-      automatic = false;
-      dates = "03:15";
     };
   };
 
