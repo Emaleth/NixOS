6136a8091aaa22711eae9a5f48e9bb3676a4d6a9
Emaleth <48322449+Emaleth@users.noreply.github.com>
2026-06-23 01:36:04 +0200

    Auto Update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd
 cd Repositories/NixOS
-sudo
-git add . && git commit -m "Auto Update" && git push
+sudo git add . && git commit -m "Auto Update" && git push
 sudo nix flake update
 cd
 sudo nixos-rebuild switch --flake /home/emaleth/Repositories/NixOS --impure
